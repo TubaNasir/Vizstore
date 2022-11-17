281b71fc597c7157211744e8b477f9722f4af3fa
TubaNasir <tubanasir135@gmail.com>
2022-11-18 00:00:05 +0500

    cleaning code

---
+++ lib/checkout/widgets/checkout_form.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutterdemo/constants.dart';
+import 'package:flutterdemo/widgets/form_field.dart';
+import 'package:flutterdemo/widgets/suffix_icon.dart';
+
+class CheckoutForm extends StatefulWidget {
+  const CheckoutForm({Key? key}) : super(key: key);
+
+  @override
+  State<CheckoutForm> createState() => _CheckoutFormState();
+}
+
+class _CheckoutFormState extends State<CheckoutForm> {
+  String dropdownvalue = 'Karachi';
+  bool enabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(13.0),
+      child: Column(children: [
+        SizedBox(height: 15),
+        CustomFormField(
+          labelText: "Name",
+          hintText: "Enter your name",
+          icon: SuffixIcon(icon: Icons.person),
+          enabled: enabled,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        CustomFormField(
+          labelText: "Contact",
+          hintText: "Enter your contact",
+          icon: SuffixIcon(icon: Icons.phone_android),
+          enabled: enabled,
+        ),
+        SizedBox(height: 20),
+        CustomFormField(
+          labelText: "Address",
+          hintText: "Enter your address",
+          icon: SuffixIcon(icon: Icons.location_on),
+          enabled: enabled,
+        ),
+        SizedBox(height: 20),
+        Container(
+          height: 65,
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(28.0),
+              border: Border.all(color: TextColor2)),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+              iconDisabledColor: Colors.white,
+              iconEnabledColor: Colors.white,
+              borderRadius: BorderRadius.circular(28),
+              isExpanded: true,
+              value: dropdownvalue,
+              icon: const Icon(
+                Icons.keyboard_arrow_down,
+                color: SecondaryColor,
+              ),
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+      ]),
+    );
+  }
+}
