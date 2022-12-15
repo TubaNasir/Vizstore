30c928222cef45c32ca52f103716910689d50293
Faakeha Ahmed <faakehaahmed123@gmail.com>
2022-12-16 01:14:47 +0500

    camera search

--- lib/controllers/camera_provider.dart
+++ lib/controllers/camera_provider.dart
@@ -1,11 +1,24 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutterdemo/models/product_model.dart';
+import 'package:flutterdemo/repositories/product_repository.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class CameraProvider with ChangeNotifier {
 
+  CameraProvider(this._productRepository);
+
+  ProductRepository _productRepository;
+
+  File? _selectedImage;
+  List<ProductJson> _similarProducts = [];
+  List<ProductJson> _products = [];
+
+  List<ProductJson> get products => _products;
+  File? get selectedImage => _selectedImage;
+  List<ProductJson> get similarProducts => _similarProducts;
 
   Future<List<dynamic>> getSimilarImages(File file, String link) async {
     ///MultiPart request
@@ -39,4 +52,35 @@ class CameraProvider with ChangeNotifier {
     print('\n\n');
     return result['SimilarImages'];
   }
+
+  Future<void> getProductsList() async {
+    print('in method');
+    _products = await _productRepository.getProductList();
+    notifyListeners();
+    print('search' + products[0].category);
+    //notifyListeners();
+  }
+
+  ProductJson getProduct(String id) {
+    ProductJson product = ProductJson.empty();
+    for (var product in _products) {
+      print("prod,, ${product.id}");
+      if(id == product.id){
+        return product;
+      }
+    }
+    return product;
+  }
+
+  List<ProductJson> setSimilarProducts(List similarImages){
+    List<ProductJson> list = [];
+    for (var element in similarImages) {
+      list.add(getProduct(element));
+    }
+
+    _similarProducts = list;
+    return _similarProducts;
+
+  }
+
 }
