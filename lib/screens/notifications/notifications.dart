bdd48dc12cc5b164073a53d5da4a0ecdd659c47a
TubaNasir <tubanasir135@gmail.com>
2022-12-18 00:56:41 +0500

    notifications

--- lib/screens/notifications/notifications.dart
+++ lib/screens/notifications/notifications.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterdemo/controllers/notifications_provider.dart';
 import 'package:flutterdemo/models/notification_model.dart';
 import 'package:provider/provider.dart';
-
 import '../widgets/custom_app_bar/custom_app_bar.dart';
-import 'notification_model.dart';
 
 class Notifications extends StatefulWidget {
   const Notifications({Key? key}) : super(key: key);
@@ -14,20 +12,18 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
-
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async => {
-      await context.read<NotificationsProvider>().getUser(),
-      context.read<NotificationsProvider>().getSortedNotifications(),
-    });
-
-
+          await context.read<NotificationsProvider>().getUser(),
+          context.read<NotificationsProvider>().getSortedNotifications(),
+        });
   }
+
   @override
   Widget build(BuildContext context) {
-
-    List<NotificationItemJson> notifications = context.watch<NotificationsProvider>().notifications;
+    List<NotificationItemJson> notifications =
+        context.watch<NotificationsProvider>().notifications;
 
     return SafeArea(
       child: Scaffold(
@@ -41,28 +37,26 @@ class _NotificationsState extends State<Notifications> {
                   scrollDirection: Axis.vertical,
                   itemCount: notifications.length,
                   itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.fromLTRB(12.0, 5.0, 12.0, 5.0),
-                    child: ListTile(
-                      dense: true,
-                      visualDensity: VisualDensity(vertical: 3),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      tileColor: Color(0xFFF5F6F9),
-                      leading: Icon(Icons.person),
-                      //store logo?
-                      title: Text(notifications[index].message,
-                      style: notifications[index].read ? null : TextStyle(
-                          fontWeight: FontWeight.bold
-                      )),
-                      trailing: Text(
-                        "${notifications[index].dateTime.day}/${notifications[index].dateTime.month} | ${notifications[index].dateTime.hour}:${notifications[index].dateTime.minute}",
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                    ),
-                  )
-              )
-          )
-      ),
+                        padding:
+                            const EdgeInsets.fromLTRB(12.0, 5.0, 12.0, 5.0),
+                        child: ListTile(
+                          dense: true,
+                          visualDensity: VisualDensity(vertical: 3),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          tileColor: Color(0xFFF5F6F9),
+                          leading: Icon(Icons.person),
+                          //store logo?
+                          title: Text(notifications[index].message,
+                              style: notifications[index].read
+                                  ? null
+                                  : TextStyle(fontWeight: FontWeight.bold)),
+                          trailing: Text(
+                            "${notifications[index].dateTime.day}/${notifications[index].dateTime.month} | ${notifications[index].dateTime.hour}:${notifications[index].dateTime.minute}",
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ),
+                      )))),
     );
   }
 }
